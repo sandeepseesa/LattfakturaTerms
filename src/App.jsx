349c30e736f2cb240a9664d11ce398e7daece5c7
sandeepseesa <sandeepseesa@gmail.com>
2025-09-18 10:14:27 +0530

    white sapce fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Terms from './pages/Terms'
 function App() {
   const [lang, setLang] = useState("English");
 
-  
 
   return (
-   <div className="App">
-      <Navbar lang={lang} setLang={setLang} />
-      <div>
-        <Terms lang={lang} setLang={setLang}/>
-      </div>
-   </div>
+    <div className="App">
+      <div className="bg"></div>   
+        <Navbar lang={lang} setLang={setLang} />
+        <div>
+          <Terms lang={lang} setLang={setLang} />
+
+        </div>
+     
+    </div>
   )
 }
 
